c2e867e1b85de42df761fc76b9931d0fae53c1fb
tubi55 <tubi55@nate.com>
2022-03-26 15:21:47 +0900

    font awesome연결

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
 	const active = { color: 'aqua' };
@@ -23,6 +25,8 @@ function Header() {
 					</NavLink>
 				</li>
 			</ul>
+
+			<FontAwesomeIcon icon={faBars} />
 		</header>
 	);
 }
