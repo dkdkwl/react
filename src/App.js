438cf7a1bd256f3970b0f925383e2ee9cb81243e
tubi55 <tubi55@nate.com>
2022-03-26 14:12:59 +0900

    기본 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Route } from 'react-router-dom';
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+import Visual from './components/main/Visual';
+import Content from './components/main/Content';
+import Youtube from './components/sub/Youtube';
+import Gallery from './components/sub/Gallery';
+
 function App() {
-	return <div className='App'></div>;
+	return (
+		<>
+			<Header />
+
+			<Route exact path='/'>
+				<Visual />
+				<Content />
+			</Route>
+
+			<Route path='/youtube' component={Youtube}></Route>
+			<Route path='/gallery' component={Gallery}></Route>
+
+			<Footer />
+		</>
+	);
 }
 
 export default App;
