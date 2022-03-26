ee7303f9f0306b669ed981fd7e996e6cd3e390eb
tubi55 <tubi55@nate.com>
2022-03-26 14:54:29 +0900

    header 스타일링

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
-	return <header>Header</header>;
+	return (
+		<header>
+			<h1>
+				<Link to='/'>LOGO</Link>
+			</h1>
+
+			<ul className='gnb'>
+				<li>
+					<Link to='/gallery'>Gallery</Link>
+				</li>
+				<li>
+					<Link to='/youtube'>Youtube</Link>
+				</li>
+			</ul>
+		</header>
+	);
 }
 
 export default Header;
