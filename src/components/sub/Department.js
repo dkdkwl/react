c088627995222b888adf5bf4e6030b67752fbf2c
tubi55 <tubi55@nate.com>
2022-03-27 12:59:06 +0900

    카운터

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function Department() {
-	return <div>Department</div>;
+	return (
+		<section className='department'>
+			<h1>Department</h1>
+
+			<button className='plus'>더하기</button>
+			<button className='minus'>빼기</button>
+			<h3>0</h3>
+		</section>
+	);
 }
 
 export default Department;
