7da1838b475fddd403dec4d088439d85334b45d5
tubi55 <tubi55@nate.com>
2022-03-27 13:59:53 +0900

    axios 데이터 요청

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,34 +1,37 @@
+import axios from 'axios';
 import React, { useState, useEffect, useRef } from 'react';
 
 function Department() {
 	let [index, setIndex] = useState(0);
 	const frame = useRef(null);
+	//추후 axios로 불러온 데이터 배열을 담을 state생성
+	const [members, setMembers] = useState([]);
+	//public폴더의 절대 경로값을 구할
+	const path = process.env.PUBLIC_URL;
+	//절대경로에서부터의 json파일 데이터 url값 구함
+	const url = `${path}/DB/department.json`;
 
 	useEffect(() => {
-		console.log('컴포넌트 생성');
 		frame.current.classList.add('on');
 
-		return () => {
-			console.log('컴포넌트 소멸');
-		};
+		//axios로 위에 만든 url로 데이터 요청후
+		//요청이 성공하면 state값을 옮겨담음
+		axios
+			.get(url)
+			.then((json) => {
+				console.log(json.data.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	}, []);
-
-	useEffect(() => {
-		console.log('index값 변경');
-	}, [index]);
 
 	return (
 		<section className='department' ref={frame}>
 			<div className='inner'>
 				<h1>Department</h1>
 
-				<button className='plus' onClick={() => setIndex(++index)}>
-					더하기
-				</button>
-				<button className='minus' onClick={() => setIndex(--index)}>
-					빼기
-				</button>
-				<h3>{index}</h3>
+				{/* state에 있는 배열값을 반복돌면서 가상DOM 생성 */}
 			</div>
 		</section>
 	);
