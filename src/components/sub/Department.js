ba490e9add0e28fa86e99034e367c14ab266c73d
tubi55 <tubi55@nate.com>
2022-03-27 14:37:55 +0900

    state값 변경 및 화면 재 랜더링

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -2,20 +2,14 @@ import axios from 'axios';
 import React, { useState, useEffect, useRef } from 'react';
 
 function Department() {
-	let [index, setIndex] = useState(0);
 	const frame = useRef(null);
-	//추후 axios로 불러온 데이터 배열을 담을 state생성
 	const [members, setMembers] = useState([]);
-	//public폴더의 절대 경로값을 구할
 	const path = process.env.PUBLIC_URL;
-	//절대경로에서부터의 json파일 데이터 url값 구함
 	const url = `${path}/DB/department.json`;
 
 	useEffect(() => {
 		frame.current.classList.add('on');
 
-		//axios로 위에 만든 url로 데이터 요청후
-		//요청이 성공하면 state값을 옮겨담음
 		axios
 			.get(url)
 			.then((json) => {
@@ -32,7 +26,16 @@ function Department() {
 			<div className='inner'>
 				<h1>Department</h1>
 
-				{/* state에 있는 배열값을 반복돌면서 가상DOM 생성 */}
+				<button
+					onClick={() => {
+						let newMembers = [...members];
+						newMembers[0].name = 'Michael';
+						setMembers(newMembers);
+						console.log(members);
+					}}>
+					멤버정보 변경
+				</button>
+
 				<ul>
 					{members.map((data, idx) => {
 						return (
