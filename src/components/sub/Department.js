328a89790ca6153a884d08f3e312fa03e129c3a7
tubi55 <tubi55@nate.com>
2022-03-27 13:10:45 +0900

    counter 완성

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Department() {
+	let [index, setIndex] = useState(0);
+
 	return (
 		<section className='department'>
 			<h1>Department</h1>
 
-			<button className='plus'>더하기</button>
-			<button className='minus'>빼기</button>
-			<h3>0</h3>
+			<button className='plus' onClick={() => setIndex(++index)}>
+				더하기
+			</button>
+			<button className='minus' onClick={() => setIndex(--index)}>
+				빼기
+			</button>
+			<h3>{index}</h3>
 		</section>
 	);
 }
