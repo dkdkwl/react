cc80a76436eacf114dfe350cc54920a3278837db
tubi55 <tubi55@nate.com>
2022-04-02 13:06:33 +0900

    리스트 클릭시 isPop state값을 변경하여 팝업 컴포넌트 출력

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -4,10 +4,11 @@ import React, { useRef, useEffect, useState } from 'react';
 function Gallery() {
 	const frame = useRef(null);
 	const [items, setItems] = useState([]);
+	const [isPop, setIsPop] = useState(false);
 
 	const api_key = 'feb5dbb632085ee9e53c197d363d1a85';
 	const method = 'flickr.interestingness.getList';
-	const per_page = 500;
+	const per_page = 3;
 	const url = `https://www.flickr.com/services/rest/?method=${method}&api_key=${api_key}&format=json&nojsoncallback=1&per_page=${per_page}`;
 
 	useEffect(() => {
@@ -25,24 +26,36 @@ function Gallery() {
 	}, []);
 
 	return (
-		<section className='gallery' ref={frame}>
-			<div className='inner'>
-				<h1>Gallery</h1>
-				<ul>
-					{items.map((item, idx) => {
-						return (
-							<li key={idx}>
-								<h2>{item.title}</h2>
-								<img
-									src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
-								/>
-							</li>
-						);
-					})}
-				</ul>
-			</div>
-		</section>
+		<>
+			<section className='gallery' ref={frame}>
+				<div className='inner'>
+					<h1>Gallery</h1>
+					<ul>
+						{items.map((item, idx) => {
+							return (
+								<li key={idx} onClick={() => setIsPop(true)}>
+									<img
+										src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
+									/>
+									<h2>{item.title}</h2>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</section>
+			{/* isPop이 ture일때 팝업출력 */}
+			{isPop ? <Popup /> : null}
+		</>
 	);
+
+	function Popup() {
+		return (
+			<aside className='popup'>
+				<span>close</span>
+			</aside>
+		);
+	}
 }
 
 export default Gallery;
