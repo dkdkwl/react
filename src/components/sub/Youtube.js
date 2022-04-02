b36232ba3bf7fa916491e2cae05ddb9b52c3e407
tubi55 <tubi55@nate.com>
2022-04-02 14:49:20 +0900

    youtbe api 호출

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,10 +1,18 @@
 import React, { useRef, useEffect } from 'react';
+import axios from 'axios';
 
 function Youtube() {
 	const frame = useRef(null);
+	const api_key = 'AIzaSyCCiJkX1nNqYL222H5m-0fCS65LfzyExlQ';
+	const play_list = 'PLHtvRFLN5v-UVVpNfWqtgZ6YPs9ZJMWRK';
+	const url = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&playlistId=${play_list}&maxResults=3&part=snippet`;
 
 	useEffect(() => {
 		frame.current.classList.add('on');
+
+		axios.get(url).then((json) => {
+			console.log(json);
+		});
 	}, []);
 
 	return (
