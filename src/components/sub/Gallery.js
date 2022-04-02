7a232715274755eab185ce7c02b503fb1d99e585
tubi55 <tubi55@nate.com>
2022-04-02 14:10:48 +0900

    팝업컴포넌트 생성시 스크롤기능 제거, 팝업 소멸시 스크롤기능 다시 활성화

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -5,11 +5,12 @@ function Gallery() {
 	const frame = useRef(null);
 	const [items, setItems] = useState([]);
 	const [isPop, setIsPop] = useState(false);
+	//목록의 순서값을 담을 state
 	const [index, setIndex] = useState(0);
 
 	const api_key = 'feb5dbb632085ee9e53c197d363d1a85';
 	const method = 'flickr.interestingness.getList';
-	const per_page = 3;
+	const per_page = 10;
 	const url = `https://www.flickr.com/services/rest/?method=${method}&api_key=${api_key}&format=json&nojsoncallback=1&per_page=${per_page}`;
 
 	useEffect(() => {
@@ -26,10 +27,6 @@ function Gallery() {
 			});
 	}, []);
 
-	useEffect(() => {
-		console.log(index);
-	}, [index]);
-
 	return (
 		<>
 			<section className='gallery' ref={frame}>
@@ -42,6 +39,7 @@ function Gallery() {
 									key={idx}
 									onClick={() => {
 										setIsPop(!isPop);
+										//리스트 클릭시 해당 리스트의 순서값으로 state변경
 										setIndex(idx);
 									}}>
 									<img
@@ -54,14 +52,19 @@ function Gallery() {
 					</ul>
 				</div>
 			</section>
-			{/* isPop이 true일때 팝업출력 */}
 			{isPop ? <Popup /> : null}
 		</>
 	);
 
 	function Popup() {
+		useEffect(() => {
+			document.body.style.overflow = 'hidden';
+			return () => (document.body.style.overflow = 'auto');
+		}, []);
+
 		return (
 			<aside className='popup'>
+				{/* index state값에 따라 팝업에 출력되는 내용 변경 */}
 				<div className='pic'>
 					<img
 						src={`https://live.staticflickr.com/${items[index].server}/${items[index].id}_${items[index].secret}_b.jpg`}
