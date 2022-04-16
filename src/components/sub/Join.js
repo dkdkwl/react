185dc10120539a95c2593ad6a74f3449818c1ee0
tubi55 <tubi55@nate.com>
2022-04-16 14:37:32 +0900

    이메일 인증 처리

--- src/components/sub/Join.js
+++ src/components/sub/Join.js
@@ -1,10 +1,13 @@
 import Layout from '../common/Layout';
 import { useEffect, useState } from 'react';
 
+//미션 -이메일 항목을 만들고 (type=text) 5글자 이상 입력시 인증통과 그렇지 않으면 에러메세지 출력
+
 function Join() {
 	const initVal = {
 		userid: '',
 		comments: '',
+		email: ''
 	};
 	const [val, setVal] = useState(initVal);
 	const [err, setErr] = useState({});
@@ -13,20 +16,23 @@ function Join() {
 		const { name, value } = e.target;
 		setVal({ ...val, [name]: value });
 	};
-	
+
 	const check = (arg) => {
-		const errs = {};	
+		const errs = {};
 		if (arg.userid.length < 5) {
 			errs.userid = '아이디를 5글자 이상 입력하세요';
 		}
-		if(arg.comments.length < 10){
+		if (arg.comments.length < 10) {
 			errs.comments = '남기는말은 10글자 이상 입력하세요';
+		}
+		if(arg.email.length < 5 || !/@./.test(val.email)){
+			errs.email = '이메일주소는 5글자이상 @ .을 포함해주세요.'
 		}
 		return errs;
 	};
 
 	const handleSubmit = (e) => {
-		e.preventDefault();	
+		e.preventDefault();
 		setErr(check(val));
 	};
 
@@ -43,6 +49,7 @@ function Join() {
 						<table border='1'>
 							<caption>회원가입</caption>
 							<tbody>
+								{/* userid */}
 								<tr>
 									<th scope='row'>
 										<label htmlFor='userid'>USER ID</label>
@@ -59,6 +66,24 @@ function Join() {
 										<span className='err'>{err.userid}</span>
 									</td>
 								</tr>
+								{/* email */}
+								<tr>
+									<th scope='row'>
+										<label htmlFor='email'>E-MAIL</label>
+									</th>
+									<td>
+										<input
+											type='text'
+											name='email'
+											id='email'
+											placeholder='이메일을 입력하세요'
+											value={val.email}
+											onChange={handleChange}
+										/>
+										<span className="err">{err.email}</span>
+									</td>
+								</tr>
+								{/* comments */}
 								<tr>
 									<th scope='row'>
 										<label htmlFor='comments'>COMMENTS</label>
@@ -71,11 +96,11 @@ function Join() {
 											rows='10'
 											placeholder='남기는말을 입력하세요'
 											value={val.comments}
-											onChange={handleChange}
-										></textarea>
-										<span className="err">{err.comments}</span>
+											onChange={handleChange}></textarea>
+										<span className='err'>{err.comments}</span>
 									</td>
 								</tr>
+								{/* btnSet */}
 								<tr>
 									<th colSpan='2'>
 										<input type='reset' value='cancel' />
