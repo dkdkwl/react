9200b5ef2b22bc9a2b0331b5abe645b334673bff
tubi55 <tubi55@nate.com>
2022-04-16 13:53:58 +0900

    인증 실패시 에러메세지를 담은 객체 생성후 err 스테이트에 저장

--- src/components/sub/Join.js
+++ src/components/sub/Join.js
@@ -1,22 +1,48 @@
 import Layout from '../common/Layout';
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import { faCheckToSlot } from '@fortawesome/free-solid-svg-icons';
 
 function Join() {
 	const initVal = {
 		userid: '',
 	}
 	const [val, setVal] = useState(initVal);
+	const [err, setErr] = useState({});
 
 	const handleChange = e =>{
 		const {name, value} = e.target;													
 		setVal({...val, [name]: value})
 	}
 
+	//인수로 현재 val의 정보값을 전달
+	const check = (arg) => {
+		const errs = {};
+		//입력된 아이디값이 5글자 이하면 에레메세지를 담아서 객체리턴
+		//입력된 아이디값이 5그라 이상이면 빈 에러객체를 리턴
+		if(arg.userid.length < 5){
+			errs.userid = '아이디를 5글자 이상 입력하세요';
+		}
+		return errs;
+	}
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		//전송 이벤트 발생시
+		//check함수로 반환된 에러객체가 err 스테이트에 저장	
+		setErr(check(val));
+	}
+
+	//미션 - 인증이 통과되면 화면에 변화없음
+	//인증 실패시 인풋 옆에 에러메세지 출력
+	useEffect(()=>{
+		console.log(err);
+	},[err]);
+
 	return (
 			
 			<Layout name={'Join'}>
 				<article>
-					<form>
+					<form onSubmit={handleSubmit}>
 						<fieldset>
 							<legend>회원가입 폼 양식</legend>
 							<table border='1'>
