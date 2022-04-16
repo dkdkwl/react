0820a208c726dbc1c476c44293cc76b1b6274218
tubi55 <tubi55@nate.com>
2022-04-16 14:08:13 +0900

    남기는말 인증처리

--- src/components/sub/Join.js
+++ src/components/sub/Join.js
@@ -1,80 +1,93 @@
 import Layout from '../common/Layout';
-import {useEffect, useState} from 'react'
-import { faCheckToSlot } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
 
 function Join() {
 	const initVal = {
 		userid: '',
-	}
+		comments: '',
+	};
 	const [val, setVal] = useState(initVal);
 	const [err, setErr] = useState({});
 
-	const handleChange = e =>{
-		const {name, value} = e.target;													
-		setVal({...val, [name]: value})
-	}
-
-	//인수로 현재 val의 정보값을 전달
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setVal({ ...val, [name]: value });
+	};
+	
 	const check = (arg) => {
-		const errs = {};
-		//입력된 아이디값이 5글자 이하면 에레메세지를 담아서 객체리턴
-		//입력된 아이디값이 5글자 이상이면 빈 에러객체를 리턴
-		if(arg.userid.length < 5){
+		const errs = {};	
+		if (arg.userid.length < 5) {
 			errs.userid = '아이디를 5글자 이상 입력하세요';
 		}
+		if(arg.comments.length < 10){
+			errs.comments = '남기는말은 10글자 이상 입력하세요';
+		}
 		return errs;
-	}
+	};
 
-	const handleSubmit = e => {
-		e.preventDefault();
-		//전송 이벤트 발생시
-		//check함수로 반환된 에러객체가 err 스테이트에 저장	
+	const handleSubmit = (e) => {
+		e.preventDefault();	
 		setErr(check(val));
-	}
+	};
 
-	
-	useEffect(()=>{
+	useEffect(() => {
 		console.log(err);
-	},[err]);
+	}, [err]);
 
 	return (
-			
-			<Layout name={'Join'}>
-				<article>
-					<form onSubmit={handleSubmit}>
-						<fieldset>
-							<legend>회원가입 폼 양식</legend>
-							<table border='1'>
-								<caption>회원가입</caption>
-								<tbody>
-									<tr>
-										<th scope='row'>
-											<label htmlFor='userid'>USER ID</label>
-										</th>
-										<td>
-											<input
-												type='text'
-												id='userid'
-												name='userid'
-												placeholder='아이디를 입력하세요'
-												value={val.userid}
-												onChange={handleChange}
-											/>
-											<span className="err">{err.userid}</span>
-										</td>
-									</tr>
-									<tr>
-										<th colSpan='2'>
-											<input type='reset' value='cancel' />
-											<input type='submit' value='send' />
-										</th>
-									</tr>
-								</tbody>
-							</table>
-						</fieldset>
-					</form>
-				</article>
-			</Layout>		
+		<Layout name={'Join'}>
+			<article>
+				<form onSubmit={handleSubmit}>
+					<fieldset>
+						<legend>회원가입 폼 양식</legend>
+						<table border='1'>
+							<caption>회원가입</caption>
+							<tbody>
+								<tr>
+									<th scope='row'>
+										<label htmlFor='userid'>USER ID</label>
+									</th>
+									<td>
+										<input
+											type='text'
+											id='userid'
+											name='userid'
+											placeholder='아이디를 입력하세요'
+											value={val.userid}
+											onChange={handleChange}
+										/>
+										<span className='err'>{err.userid}</span>
+									</td>
+								</tr>
+								<tr>
+									<th scope='row'>
+										<label htmlFor='comments'>COMMENTS</label>
+									</th>
+									<td>
+										<textarea
+											name='comments'
+											id='comments'
+											cols='30'
+											rows='10'
+											placeholder='남기는말을 입력하세요'
+											value={val.comments}
+											onChange={handleChange}
+										></textarea>
+										<span className="err">{err.comments}</span>
+									</td>
+								</tr>
+								<tr>
+									<th colSpan='2'>
+										<input type='reset' value='cancel' />
+										<input type='submit' value='send' />
+									</th>
+								</tr>
+							</tbody>
+						</table>
+					</fieldset>
+				</form>
+			</article>
+		</Layout>
 	);
 }
 
