288a7b9e7c367e55389625a8afe113cb61cc7c17
tubi55 <tubi55@nate.com>
2022-04-23 12:52:43 +0900

    수정 버튼 클릭시 해당 순번의 state값만 enableUpate=true라는 객체값 추가

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -6,55 +6,61 @@ function Community() {
 	const textarea = useRef(null);
 
 	const dummyPosts = [
-    { title: 'Hello6', content: 'Here comes description in detail.' },
+		{ title: 'Hello6', content: 'Here comes description in detail.' },
 		{ title: 'Hello5', content: 'Here comes description in detail.' },
-    { title: 'Hello4', content: 'Here comes description in detail.' },
+		{ title: 'Hello4', content: 'Here comes description in detail.' },
 		{ title: 'Hello3', content: 'Here comes description in detail.' },
 		{ title: 'Hello2', content: 'Here comes description in detail.' },
 		{ title: 'Hello1', content: 'Here comes description in detail.' },
 	];
 	const [posts, setPosts] = useState(dummyPosts);
 
-  const resetPosts  = () =>{
-    input.current.value='';
-    textarea.current.value='';
-  }
-  const createPosts = () => {
-    const inputVal = input.current.value.trim();
-    const textareaVal = textarea.current.value.trim();
+	const resetPosts = () => {
+		input.current.value = '';
+		textarea.current.value = '';
+	};
 
-    if( !inputVal || !textareaVal ){
-      alert('제목과 본문을 모두 입력하세요');
-      return;
-    }
+	const createPosts = () => {
+		const inputVal = input.current.value.trim();
+		const textareaVal = textarea.current.value.trim();
+		resetPosts();
 
-    setPosts([
-      { title: inputVal, content: textareaVal },
-      ...posts,
-    ]);
-  }
+		if (!inputVal || !textareaVal) {
+			alert('제목과 본문을 모두 입력하세요');
+			return;
+		}
 
-  const deleletPosts = (idx) =>{                
-    setPosts(     
-      posts.filter((post, index) => index !== idx)
-    );
-  }
+		setPosts([{ title: inputVal, content: textareaVal }, ...posts]);
+	};
+
+	const deleletPosts = (idx) => {
+		setPosts(posts.filter((_, index) => index !== idx));
+	};
+
+	//수정모드 변경함수
+	const enableUpdate = (index) => {
+		setPosts(
+			posts.map((post, idx) => {
+				if (idx === index) post.enableUpdate = true;
+				return post;
+			})
+		);
+	};
+
+	useEffect(() => {
+		console.log(posts);
+	}, [posts]);
 
 	return (
 		<Layout name={'Community'}>
 			<div className='inputBox'>
-				<input 
-          type='text' 
-          placeholder='제목을 입력하세요' 
-          ref={input} 
-        />
+				<input type='text' placeholder='제목을 입력하세요' ref={input} />
 				<br />
 				<textarea
 					cols='30'
 					rows='10'
 					placeholder='본문을 입력하세요'
-          ref={textarea}
-        ></textarea>
+					ref={textarea}></textarea>
 				<br />
 				<button onClick={resetPosts}>cancel</button>
 				<button onClick={createPosts}>create</button>
@@ -67,9 +73,8 @@ function Community() {
 							<h2>{post.title}</h2>
 							<p>{post.content}</p>
 
-              <button>edit</button>
-              {/* 삭제 클릭시 deletePosts함수에 삭제할 순번을 전달하면서 호출 */}
-              <button onClick={()=>deleletPosts(idx)}>delete</button>
+							<button onClick={() => enableUpdate(idx)}>edit</button>
+							<button onClick={() => deleletPosts(idx)}>delete</button>
 						</article>
 					);
 				})}
